f6b584ccd5c875c697992ffc7fc462920c2a17ac
Aymane Aaraba <101283526+ayman-tra@users.noreply.github.com>
2023-11-30 12:04:44 +0100

    libft

---
+++ libft/ft_memcmp.c
@@ -0,0 +1,38 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_memcmp.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: aaaraba <aaaraba@student.42.fr>            +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/11/04 21:24:32 by aaaraba           #+#    #+#             */
+/*   Updated: 2023/11/29 15:11:45 by aaaraba          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+#include <string.h>
+
+int	ft_memcmp(const void *s1, const void *s2, size_t n)
+{
+	char	*p1;
+	char	*p2;
+	size_t	i;
+
+	i = 0;
+	p1 = (char *)s1;
+	p2 = (char *)s2;
+	while (i < n)
+	{
+		if (p1[i] != p2[i])
+			return ((unsigned char)p1[i] - (unsigned char)p2[i]);
+		i++;
+	}
+	return (0);
+}
+int main ()
+{
+	int i[] = {1028, 2, 3};
+	int s[] = {4, 5, 2, 3};
+	printf("%d", memcmp(i, s, 4));
+}
---
+++ libft/ft_memset.c
@@ -0,0 +1,35 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_memset.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: aaaraba <aaaraba@student.42.fr>            +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/10/31 21:08:28 by aaaraba           #+#    #+#             */
+/*   Updated: 2023/11/29 17:04:52 by aaaraba          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+void	*ft_memset(void *b, int c, size_t len)
+{
+	size_t			i;
+	unsigned char	*s;
+
+	i = 0;
+	s = (unsigned char *)b;
+	while (i < len)
+	{
+		s[i] = (unsigned char)c;
+		i++;
+	}
+	return (b);
+}
+int main ()
+{
+	char	s[] = "hellohfakshfadshfdsghkfg";
+	int i = 2004;
+	
+	printf("%s", ft_memset(s, i, 10));
+}
